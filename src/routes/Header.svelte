4b39304b8010058fe6496908238596e8189600ef
Abraham Menendez <menendezabraham@gmail.com>
2023-11-14 22:47:10 +0100

    Base project

---
+++ src/routes/Header.svelte
@@ -0,0 +1,19 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div id="header-container">
+	<span id="title">Hi!</span>
+	<header>
+		<nav>
+			<ul>
+				<li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+					<a href="/">👨🏻</a>
+				</li>
+				<li>
+					<span id="modeToggle">🌕</span>
+				</li>
+			</ul>
+		</nav>
+	</header>
+</div>
---
+++ tests/page.test.ts
@@ -0,0 +1,9 @@
+//import { expect, test } from '@playwright/test';
+
+//test('Layout has expected header.', async ({ page }) => {
+//await page.goto('/');
+//await expect(page.locator('p').first()).toHave;
+//});
+
+// tests
+// deploy to /svelte-version
