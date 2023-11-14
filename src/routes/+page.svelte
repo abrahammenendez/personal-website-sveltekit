2e3859660ede73767016a634aeeea9ad87b5989b
Abraham Menendez <menendezabraham@gmail.com>
2023-11-14 22:47:10 +0100

    Base project.

---
+++ src/routes/+page.svelte
@@ -0,0 +1,154 @@
+<p>
+	I'm Abraham Menéndez, a 30-year-old Spanish software developer based in Amsterdam (Netherlands).
+</p>
+
+<p>
+	If you are here, you probably already know that I love building software. These days I'm doing it
+	at
+	<a class="current-job" href="https://gorillas.io/en" target="_blank" rel="noopener noreferrer"
+		>Gorillas</a
+	>
+	as a Checkout Tech Lead. Prior to joining them, I was a Team Lead at
+	<a class="previous-job" href="https://www.sngular.com/" target="_blank" rel="noopener noreferrer"
+		>Sngular</a
+	>. You can check out my full work history
+	<a
+		class="job-history"
+		href="https://www.linkedin.com/in/abraham-menendez"
+		target="_blank"
+		rel="noopener noreferrer">here</a
+	>.
+</p>
+
+<p>
+	As I strongly believe in giving back, I'm also trying to be a valuable Mentor at <a
+		class="oss"
+		href="https://exercism.com/"
+		target="_blank"
+		rel="noopener noreferrer">Exercism</a
+	>, a not-for-profit organization that teaches programming.
+</p>
+
+<p>
+	I'm not really into social media (there are too many distractions nowadays), but I do have a <a
+		class="short-form-social"
+		href="https://twitter.com/whoisabraham"
+		target="_blank"
+		rel="noopener noreferrer">Twitter</a
+	> that I mostly use to retweet other people's thoughts. I'm exploring different platforms to share
+	more in-depth thoughts, but I haven't found the right one yet.
+</p>
+
+<p>
+	Although I don't have as much free time as I would like, I usually try to develop
+	<a
+		class="repositories"
+		href="https://github.com/abrahammenendez"
+		target="_blank"
+		rel="noopener noreferrer">little projects</a
+	>
+	on my own time. Obviously, most of them end up in the graveyard of unfinished stuff. This year I think
+	I will finally finish something worth talking about, so let's keep in touch, OK?
+</p>
+
+<p>
+	See you around, <br />
+	<a class="email" href="mailto:menendezabraham@gmail.com" target="_top">Abraham</a>
+</p>
+
+<style>
+	/* //////////////// LINKS ////////////////// */
+
+	a:link,
+	a:visited {
+		text-decoration: none;
+		cursor: pointer;
+	}
+
+	a:hover {
+		text-decoration: underline;
+	}
+
+	.linkedin {
+		color: #de6037;
+	}
+
+	.current-job {
+		color: #d32c3b;
+	}
+
+	.previous-job {
+		color: #cc9900;
+	}
+
+	.job-history {
+		color: #e95ea7;
+	}
+
+	.oss {
+		color: #28a745;
+	}
+
+	.short-form-social {
+		color: #2e91fd;
+	}
+
+	.repositories {
+		color: #8e47f9;
+	}
+
+	.email {
+		color: #00adc8;
+	}
+
+	/* //////////////// ADDITIONAL COLORS ////////////////// */
+
+	.dominion {
+		color: #719fd1;
+	}
+
+	.color-1 {
+		color: #ff5733;
+		/* Vivid orange */
+	}
+
+	.color-2 {
+		color: #5dbcd2;
+		/* Light blue */
+	}
+
+	.color-3 {
+		color: #ffcb05;
+		/* Bright yellow */
+	}
+
+	.color-4 {
+		color: #7cbb77;
+		/* Light green */
+	}
+
+	.color-5 {
+		color: #ff75a0;
+		/* Pink */
+	}
+
+	.color-6 {
+		color: #4d4d4d;
+		/* Gray */
+	}
+
+	.color-7 {
+		color: #009688;
+		/* Teal */
+	}
+
+	.color-8 {
+		color: #d68e26;
+		/* Gold */
+	}
+
+	.color-9 {
+		color: #5323e8;
+		/* Purple */
+	}
+</style>
---
+++ src/routes/Header.svelte
@@ -0,0 +1,54 @@
+<script>
+	import { page } from '$app/stores';
+</script>
+
+<div id="header-container">
+	<span id="title">Hi!</span>
+	<header>
+		<nav>
+			<ul>
+				<!-- <li aria-current={$page.url.pathname === '/' ? 'page' : undefined}>
+					<a href="/">👨🏻</a>
+				</li> -->
+				<li>
+					<span id="modeToggle">🌕</span>
+				</li>
+			</ul>
+		</nav>
+	</header>
+</div>
+
+<style>
+	/* //////////////// HEADER, NAV ////////////////// */
+
+	#header-container {
+		margin-top: 25px;
+		display: flex;
+		justify-content: space-between;
+		align-self: flex-start;
+		font-family: 'Inter-Bold', 'Arial', sans-serif;
+		font-size: 1.5em;
+	}
+
+	#title {
+		align-self: flex-start;
+	}
+
+	header {
+		align-self: flex-end;
+	}
+
+	nav ul {
+		list-style: none;
+		display: flex;
+		align-items: center;
+		gap: 15px;
+		margin-top: 0;
+		margin-bottom: 0;
+	}
+
+	nav ul li {
+		text-decoration: none;
+		cursor: pointer;
+	}
+</style>
